e9e2ddd34bc7aafa819f337f6f61071d742881f5
ramazanncicek <ramazanncicek@gmail.com>
2023-02-01 05:13:07 +0300

    Pagination added
    -Pagination added.
    -useEffect added for filters.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,10 @@ function App() {
   const [countryByArea,setByArea] = useState([]);
   const [displayCountries,setDisplayCountries] = useState('');
   const [ascending,setAscending] = useState(true);
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const itemsPerPage = 3;
-  // const indexOfLastItem = currentPage * itemsPerPage;
-  // const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  // const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const lastItemDisplayed = currentPage * itemsPerPage;
+  const firstItemDisplayed = lastItemDisplayed - itemsPerPage;
 
   useEffect(()=>{
     fetch('https://restcountries.com/v2/all?fields=name,region,area')
@@ -23,7 +22,15 @@ function App() {
       setCountries(final);
       setDisplayCountries(final);
     });
-  },[])
+  },[]);
+
+  useEffect(() => {
+  setDisplayCountries(smallerCountries);
+  },[smallerCountries]);
+
+  useEffect(() => {
+  setDisplayCountries(countryByArea);
+  },[countryByArea]);
 
   const descendingList = () => {
     setDisplayCountries([...displayCountries].reverse());
@@ -32,16 +39,23 @@ function App() {
 
   const smallerByArea = () => {
     allCountries.find((country) =>{
-      if(country.name == "Lithuania") setSmallerCountries(allCountries.filter((area) => area.area < country.area))
+      if(country.name === "Lithuania") setSmallerCountries(allCountries.filter((area) => area.area < country.area))
     });
-    setDisplayCountries(smallerCountries);
-    // Need to make it sync with callback
   }
   const unitByRegion = () => {
-    setByArea(allCountries.filter((item) => item.region == "Oceania"));
-    setDisplayCountries(countryByArea);
-    // Need to make it sync with callback
+    setByArea(allCountries.filter((item) => item.region === "Oceania"));
   }
+
+  const pageUp = () => {
+    if(currentPage === Math.ceil(displayCountries.length / itemsPerPage)) setCurrentPage(1);
+    else setCurrentPage(currentPage+1);
+  };
+
+  const pageDown = () => {
+    if(currentPage>1) setCurrentPage(currentPage-1);
+    else setCurrentPage(Math.ceil(displayCountries.length / itemsPerPage));
+  };
+
   return (
     <Container className="App" fluid>
       <Row style={{margin: "5px 5px 10px 0", width:"fit-content"}}>
@@ -51,14 +65,17 @@ function App() {
         <Col className='d-flex justify-content-start'>
           <button onClick={smallerByArea} className="country-btn">Smaller than Lithuania</button>
           <button onClick={unitByRegion} style={{marginLeft:"20px"}} className="country-btn">Inside 'Ocean' Region</button>
+          <button onClick={pageDown} style={{marginLeft:"20px"}} className="country-btn">Page Down</button>
+          <h3 style={{marginLeft:"10px"}}>{currentPage}</h3>
+          <button onClick={pageUp} style={{marginLeft:"10px"}} className="country-btn">Page Up</button>
         </Col>
         <Col className='d-flex justify-content-end'>
           {ascending ? <button onClick={descendingList} className="country-btn">Descending</button> : <button onClick={descendingList} className="country-btn">Ascending</button>}
         </Col>
       </Row>
-      {displayCountries.length > 0 ? displayCountries.map((country,index) => (
-        <CountryCard name={country.name} region={country.region} area={country.area} key={index} ></CountryCard>
-      )) : null}
+      {displayCountries.length > 0 ? displayCountries.map((country,index) =>{
+        if (index < lastItemDisplayed && index >= firstItemDisplayed) return <CountryCard name={country.name} region={country.region} area={country.area} key={index}></CountryCard>;
+      }) : null}
     </Container>
   );
 }
